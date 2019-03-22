7ebc0539c8b10679a57ed94076b383d81d86a4bc
Keegan Quinn <keeganquinn@gmail.com>
2019-03-22 14:05:41 -0700

    Switch to CDN hosted Bootstrap.

--- js/page.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function() {
-  // Open all external links in new tabs/windows
-  $("a[href^='http://']").attr("target", "_blank");
-  $("a[href^='https://']").attr("target", "_blank");
-});
